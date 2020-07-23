e89dc28d81b152d3d9bd574e36374c6b8aa65cfc
Ben Sultan <bensultan@Bens-MacBook-Pro.local>
2020-07-22 20:57:42 -0700

    added and subtracted firebase

--- public/script.js
+++ public/script.js
@@ -3,11 +3,15 @@ let querySubmit = document.getElementById('query-submit')
 let results = document.getElementById('results')
 let content = document.getElementById('content')
 
-const getQuery = () => {
+//sends query and receives results
+const getQuery = (e) => {
+    e.preventDefault()
+    //clears previous results, if any
     results.innerHTML = '';
     const queryString = queryInput.value;
     console.log(queryString);
     (async () => {
+        //posts query string to the back end
         const rawResponse = await fetch('/getrequest', {
         method: 'POST',
         headers: {
@@ -16,12 +20,15 @@ const getQuery = () => {
         },
         body: JSON.stringify({'queryString': queryString})
         });
+        //receives results from the back end
         const data = await rawResponse.json();
         console.log(data);
+        //creates results divs
         for (let i = 0; i < data.length; i++) {
             let newResult = document.createElement('div');
             newResult.className = 'results-li'
-            newResult.innerHTML = `<div class="series">series: ${data[i].series_title}</div><div class="release">VHS title: ${data[i].release_title}</div><div class="year">release year: ${data[i].release_year}</div><div class="summary">summary: ${data[i].summary}</div><div>The current estimated value of this VHS is: $${data[i].price_value}</div>`
+            newResult.innerHTML = `<img class="results-image" src="./images/img${data[i].id}.jpg" /><div class="series">series: ${data[i].series_title}</div><div class="release">VHS title: ${data[i].release_title}</div><div class="year">release year: ${data[i].release_year}</div><div class="summary">summary: ${data[i].summary}</div><div>The current estimated value of this VHS is: $${data[i].price_value}</div>`
+            //result div is added to the DOM
             results.appendChild(newResult)
         }
     })();
